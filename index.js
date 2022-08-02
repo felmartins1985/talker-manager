fc78a25ffe73bb9ee2946265806532c3d81e6889
Felipe <felmartins1985@gmail.com>
2022-08-02 17:55:01 -0300

    feat: requisito 4- verifico email e password

--- index.js
+++ index.js
@@ -32,8 +32,23 @@ app.get('/talker/:id', async (req, res) => {
   }
   return res.status(200).json(findTalker);
 });
-// requisito 3
+// requisito 3 e 4
 app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!regexEmail.test(email)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    return res.status(400)
+    .json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
   const token = crypto.randomBytes(8).toString('hex');
   return res.status(200).json({ token });
 });
