8602c5497240563ca536bfb58d30dd8143f596fa
Felipe <felmartins1985@gmail.com>
2022-08-02 17:09:01 -0300

    feat: requisito 1- rota para retornar os talkers

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const fs = require('fs').promises;
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
-
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+// req 1
+app.get('/talker', async (req, res) => {
+    const readTalkers= await fs.readFile('talker.json', 'utf8');
+    const talkers = JSON.parse(readTalkers);
+    if (!talkers || talkers.length === 0) {
+     return  res.status(200).json([]);
+    }
+    return  res.status(200).json(talkers);
+})
 
 app.listen(PORT, () => {
   console.log('Online');
